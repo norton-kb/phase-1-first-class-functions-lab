a0a3bdff2a5fefe530b0fa74ce08e3d9b463e92c
norton-kb <biinorton080@gmail.com>
2024-10-24 00:23:29 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function(){
+    return ["Antonia", "Nuru"];
+}
+const returnLastTwoDrivers = function() {
+    return ["Amari", "Mo"];
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(multiplier) {
+        return function(fare) {
+            return multiplier*fare
+        }
+
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers (arrayOfDrivers, selectionfunction) {
+    return selectionfunction(drivers);
+}
